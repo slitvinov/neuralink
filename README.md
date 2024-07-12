973d9704f86c0552bf77778ffea930dca11f8d73
Sergey Litvinov <slitvinov@gmail.com>
2024-07-12 17:42:44 +0200

    up

---
+++ README.md
@@ -0,0 +1,7 @@
+# Intro
+
+https://content.neuralink.com/compression-challenge/README.html
+
+
+<p align="center"><img src="img/full.svg"/></p>
+<p align="center"><img src="img/full.png"/></p>
--- main.py
+++ main.py
@@ -1,31 +1,54 @@
+#!/usr/bin/env python
+
 import struct
-
-
-def unpack(fmt, stream):
-    return struct.unpack(fmt, f.read(struct.calcsize(fmt)))
-
+import sys
+import array
+import collections
 
 WAVE_FORMAT_PCM = 0x0001
+def unpack(fmt, stream):
+    return struct.unpack(fmt, stream.read(struct.calcsize(fmt)))
 
-path = "data/0052503c-2849-4f41-ab51-db382103690c.wav"
-with open(path, "rb") as f:
-    ckID, = unpack("4s", f)
-    assert ckID == b"RIFF"
-    cksize, = unpack("<I", f)
-    WAVEID, = unpack("4s", f)
-    assert WAVEID == b"WAVE"
-    n = cksize - 4
-    ckID, = unpack("4s", f)
-    assert ckID == b"fmt "
-    cksize, = unpack("<I", f)
-    assert cksize == 16
-    wFormatTag, = unpack("<H", f)
-    if wFormatTag != WAVE_FORMAT_PCM:
-        sys.stderr.write("main.py: error: only PCM Format is supported\n")
-    nChannels, nSamplesPerSec, nAvgBytesPerSec, nBlockAlign, wBitsPerSample, cbSize, wValidBitsPerSample, dwChannelMask = unpack(
-        "<HIIHHHHI", f)
-    SubFormat = unpack("16c", f)
-    nbytes = n - 48
-    b = f.read(nbytes)
-    print(nChannels, wBitsPerSample, nBlockAlign, cbSize, wValidBitsPerSample,
-          dwChannelMask)
+def read(path):
+    with open(path, "rb") as f:
+        ckID, = unpack("4s", f)
+        assert ckID == b"RIFF"
+        cksize0, = unpack("<I", f)
+        WAVEID, = unpack("4s", f)
+        assert WAVEID == b"WAVE"
+        ckID, = unpack("4s", f)
+        assert ckID == b"fmt "
+        cksize, = unpack("<I", f)
+        assert cksize == 16
+        wFormatTag, = unpack("<H", f)
+        if wFormatTag != WAVE_FORMAT_PCM:
+            sys.stderr.write("main.py: error: only PCM Format is supported\n")
+            sys.exit(1)
+        nChannels, nSamplesPerSec, nAvgBytesPerSec, nBlockAlign, wBitsPerSample \
+             = unpack("<HIIHH", f)
+        if nChannels != 1:
+            sys.stderr.write("main.py: error: only one channel is supported\n")
+            sys.exit(1)
+        ckID, = unpack("4s", f)
+        assert ckID == b"data"
+        cksize, = unpack("<I", f)
+        M = nAvgBytesPerSec // nSamplesPerSec // nChannels
+        Ns = cksize // nChannels // M # number of blocks
+        assert nAvgBytesPerSec == nSamplesPerSec * M * nChannels
+        assert nBlockAlign == M * nChannels
+        assert 4 + 24 + (8 + M * nChannels * Ns) == cksize0
+        a = array.array("H", f.read())
+        assert len(a) == Ns
+        return a
+
+# a = read("data/006c6dd6-d91e-419c-9836-c3f320da4f25.wav")
+a = read(sys.argv[1])
+# c = collections.Counter(a)
+# print(len(c))
+# exit(0)
+p = None
+for i, x in enumerate(a):
+    if p == None or abs(x - p) > 10000:
+        print("")
+    print(i, x)
+    p = x
