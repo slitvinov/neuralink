67d3287d4c4e345f0d8e3ffba1321e78d59e42fa
Sergey Litvinov <slitvinov@gmail.com>
2024-07-15 19:20:11 +0200

    map uniq

--- hist.py
+++ hist.py
@@ -1,12 +1,34 @@
 #!/usr/bin/env python
 
 import sys
-import collections
-import itertools
+import struct
 import mwave
+import itertools
 
-a = mwave.read(sys.argv[1])
-c = sorted(collections.Counter(a))
-c = collections.Counter(y - x for x, y in itertools.pairwise(c))
-for k, v in sorted(c.items()):
-    print(k, v)
+def min_max(fmt):
+    for x in itertools.count():
+        try:
+            struct.pack(fmt, x)
+        except struct.error:
+            maxx = x - 1
+            break
+    for x in itertools.count(step=-1):
+        try:
+            struct.pack(fmt, x)
+        except struct.error:
+            minx = x + 1
+            break
+    return minx, maxx
+
+u, v = min_max("<h")
+p, q = -512, 512
+s = set()
+for path in sys.argv[1:]:
+    a = mwave.read(path)
+    s.update(a)
+D = { }
+for x in sorted(s):
+    a = (x - u) * (q - p) // (v - u) + p
+    if a in D:
+        print(x, D[a], a)
+    D[a] = x
